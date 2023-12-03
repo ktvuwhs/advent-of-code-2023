52c61ae10c885456302c58648fe9e149c1d8f7aa
kylenvu <ktvu@hbuhsd.edu>
2023-12-03 12:07:33 -0800

    comments and formatting

--- day2/Day2.java
+++ day2/Day2.java
@@ -13,11 +13,12 @@ public class Day2
         final int RED_MAX   = 12;
         final int GREEN_MAX = 13;
         final int BLUE_MAX  = 14;
-        int gameID = 1;
-        int sumGameID = 0;  // Part 1 answer
-        int sumPower = 0;   // Part 2 answer
-        File inFile = new File("day2-input.txt");
-        Scanner sc = new Scanner(inFile);
+
+        int gameID    = 1;
+        int sumGameID = 0;   // Part 1 answer
+        int sumPower  = 0;   // Part 2 answer
+        File inFile   = new File("day2-input.txt");
+        Scanner sc    = new Scanner(inFile);
 
         // Used to read "num color" for an entire game
         final Pattern PAT_NUM_AND_COLOR = Pattern.compile("[0-9]+ red|[0-9]+ blue|[0-9]+ green", Pattern.CASE_INSENSITIVE);
@@ -32,15 +33,15 @@ public class Day2
             matchLine = PAT_NUM_AND_COLOR.matcher(sc.nextLine());
             boolean validGame = true;
 
-            int maxRed = Integer.MIN_VALUE;
+            int maxRed   = Integer.MIN_VALUE;
             int maxGreen = Integer.MIN_VALUE;
-            int maxBlue = Integer.MIN_VALUE;
+            int maxBlue  = Integer.MIN_VALUE;
             while (matchLine.find())
             {
                 // System.out.println(matchLine.group());
                 // Gets a single "num color" from the game
-                matchCube = PAT_NUM_OR_COLOR.matcher(matchLine.group());
-                int amtCube = 0;
+                matchCube    = PAT_NUM_OR_COLOR.matcher(matchLine.group());
+                int amtCube  = 0;
                 String color = null;
 
                 // First match is always the number; second is always the color due to nature of PAT_NUM_OR_COLOR
@@ -48,16 +49,16 @@ public class Day2
                 if (matchCube.find()) { color = matchCube.group(); }
 
                 // Part 1 - Counting valid games
-                boolean validRed = color.equals("red")   && amtCube <= RED_MAX;
+                boolean validRed   = color.equals("red")   && amtCube <= RED_MAX;
                 boolean validGreen = color.equals("green") && amtCube <= GREEN_MAX;
-                boolean validBlue = color.equals("blue")  && amtCube <= BLUE_MAX;
+                boolean validBlue  = color.equals("blue")  && amtCube <= BLUE_MAX;
                 
                 if (!(validRed || validGreen || validBlue)) { validGame = false; }
                 
                 // Part 2 - Getting minimum cubes that can play the game
-                if (color.equals("red") && amtCube > maxRed) { maxRed = amtCube; }
+                if (color.equals("red")   && amtCube > maxRed)   { maxRed = amtCube; }
                 if (color.equals("green") && amtCube > maxGreen) { maxGreen = amtCube; }
-                if (color.equals("blue") && amtCube > maxBlue) { maxBlue = amtCube; }
+                if (color.equals("blue")  && amtCube > maxBlue)  { maxBlue = amtCube; }
             }
 
             if (validGame) { sumGameID += gameID; }
